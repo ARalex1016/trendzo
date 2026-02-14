8492cc53e1dba9909604b8997d6cb4c429cb598c
Aslam <aralex1016@gmail.com>
2026-02-14 03:34:20 +0200

    Modify Axios to handle Production Server URL

--- client/src/store/axios.ts
+++ client/src/store/axios.ts
@@ -1,8 +1,8 @@
 import axios from "axios";
 
-const serverUrl = import.meta.env.VITE_SERVER_URL || "http://localhost:5000";
+const serverUrl = import.meta.env.VITE_SERVER_URL;
 
 export const axiosInstance = axios.create({
-  baseURL: import.meta.env.MODE === "development" ? `${serverUrl}/api` : "/api",
+  baseURL: `${serverUrl}/api`,
   withCredentials: true,
 });
