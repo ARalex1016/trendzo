179ec27a3ac850eb4d9e48f6f22495439b50efc2
Aslam <aralex1016@gmail.com>
2026-02-18 03:41:07 +0200

    Server URL Test 1

--- client/src/store/axios.ts
+++ client/src/store/axios.ts
@@ -2,6 +2,8 @@ import axios from "axios";
 
 const serverUrl = import.meta.env.VITE_SERVER_URL;
 
+console.log(serverUrl);
+
 export const axiosInstance = axios.create({
   baseURL: `${serverUrl}/api`,
   withCredentials: true,
