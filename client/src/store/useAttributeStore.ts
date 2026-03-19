8094321f749166570fc3808567d2fa3712accc43
Aslam <aralex1016@gmail.com>
2026-03-19 04:30:41 +0200

    Trail Server Bug

--- client/src/store/useAttributeStore.ts
+++ client/src/store/useAttributeStore.ts
@@ -26,12 +26,18 @@ const useAttributeStore = create<AttributeStore>((set) => ({
   attributes: null,
 
   getAttributes: async () => {
+    console.log(1);
+
     try {
       let res = await axiosInstance.get("/v1/attributes");
+      console.log(2);
+      console.log(res.data.data);
 
       set({ attributes: res.data.data });
     } catch (error: unknown) {
       const err = error as AxiosError<ApiErrorResponse>;
+      console.log(3);
+      console.log(err.response?.data?.message);
 
       throw new Error(
         err.response?.data?.message || "Coupon validation failed",
